a1adbee2ef9a3a8b1a7db332d6c656149c00d20b
Abhi7012 <72123024+Abhi7012@users.noreply.github.com>
2022-04-08 15:59:22 +0530

    first commit to portfolio
    Hey this is my portfolio
    Do contact me for any queries

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Nav from './nav';
+import BodyMain from './bodyMain';
+import Footer from './footer';
+
+
+function App() {
+    return (
+        <div className="App">
+            <Nav />
+            <BodyMain />
+            <Footer />
+
+        </div>
+    );
+}
+export default App;
